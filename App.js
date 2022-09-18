f94ec4f72c2dd9f072782733f9c827492d72bce2
Jarrian <glorianagojar@gmail.com>
2022-09-18 09:16:22 +0800

    added screens

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import styles from './assets/styles/global';
+import Home from './assets/screens/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
