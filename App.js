90a70f1f94d4f6b96c140b82e0c5f2c19e099ec5
Jarrian <glorianagojar@gmail.com>
2022-09-18 09:50:32 +0800

    fonts added

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import styles from './assets/styles/global';
 import Home from './screens/home';
+import * as Font from 'expo-font';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
 
 export default function App() {
   return (
