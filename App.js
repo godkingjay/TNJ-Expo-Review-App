49c5352cecff1d29ac5a7d138ddabadc05a55158
Jarrian <glorianagojar@gmail.com>
2022-09-18 11:20:55 +0800

    added fontloaded validation

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
+import React , { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import styles from './assets/styles/global';
 import Home from './screens/home';
 import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const getFonts = () => Font.loadAsync({
-    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
-  });
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await getFonts();
+        await new Promise(resolve => setTimeout(resolve, 2));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
-    <Home />
+    <View
+      styles={styles.screen}
+      onLayout={onLayoutRootView}
+    >
+      <Home />
+    </View>
   );
 }
--- assets/styles/global.js
+++ assets/styles/global.js
@@ -2,6 +2,11 @@ import React from "react";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
     backgroundColor: "white",
